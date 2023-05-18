cf72aa3700c9aa951a3b93c3109a26042fba8f8c
Aia_makhruka <aia.makhruka@gmail.com>
2023-05-17 22:57:37 -0400

    creates the videoplayer folder

---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,14 @@
+
+
+function VideoPlayer(){
+    return(
+        <>
+        <section className="video-player-container">
+        <video className="video" poster="">
+        </video>
+        </section>
+        </>
+    )
+}
+
+export default VideoPlayer;
