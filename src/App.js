25172379a245877f02e276e3efaef05d3ffb0116
Aia_makhruka <aia.makhruka@gmail.com>
2023-05-22 23:57:43 -0400

    editing the Navigation-bar and the desktoplayout in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.scss';
+import PlayNextVideos from './data/videos.json'
+import  { useState } from 'react';
+import CurrentVideos from './data/video-details.json';
 import NavBar from './components/NavBar/NavBar';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import VideoPlayerDetail from './components/VideoPlayerDetail/VideoPlayerDetail';
+import VideosList from './components/VideosList/VideosList';
+import CommentSection from './components/CommentSection/CommentSection';
 
 function App() {
+
+const [CurrentVideo , setCurrentVideo] = useState(CurrentVideos[0]);
+const changeCurrentVideo = (VideoDataId)=>{
+  const newVideo= CurrentVideos.find((video)=>{
+    console.log(video,VideoDataId)
+    return  video.id===VideoDataId 
+  } )
+
+  setCurrentVideo(newVideo)
+}
+
+const fillteredArray =PlayNextVideos.filter((video)=>{
+  return video.id!==CurrentVideo.id
+})
+
   return (
     <>
       <NavBar/>
+      <VideoPlayer PlayingVideo={CurrentVideo}/>
+      <main className='main-page'>
+      <section className='main-page__left'>
+      <VideoPlayerDetail VideoDetails={CurrentVideo}/>
+      <CommentSection CommentsDetails={CurrentVideo.comments}/>
+      </section>
+      <section className='main-page__right'>
+      <VideosList VideosListData={fillteredArray} changeCurrentVideo={changeCurrentVideo}/>
+      </section>
+      </main>
     </>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,6 @@
 import "./NavBar.scss";
 import avatar from '../../assets/images/Mohan-muruge.jpg';
 import logo from '../../assets/logo/BrainFlix-logo.svg';
-import searchIcon from '../../assets/icons/search.svg';
 import uploadIcon from '../../assets/icons/upload.svg';
 
 function NavBar() {
@@ -10,11 +9,12 @@ function NavBar() {
     <nav className="nav-bar">
         <a href="/"><img className="nav-bar__logo" src={logo} alt="brainflix logo"/></a>
         <form className="nav-bar__container">
-           <div className="search-bar-container">   {/* <------ is my BEM good ? */}
-            <input className="search-bar" type="search" placeholder="Search"/>
+           <div className="search-bar-container">  
+           <label htmlFor="search" ></label>
+            <input className="search-bar" type="search" id="search" placeholder="Search"/>
             <img className="profile-picture" src={avatar} alt="the profile"/>
             </div>
-            <button className="upload-button">
+            <button type="submit" className="upload-button">
             <img className="upload-button__icon" src={uploadIcon}  alt="upload icon in the upload button"/>
             UPLOAD
             <div></div>
