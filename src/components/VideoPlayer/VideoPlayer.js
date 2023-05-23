d5bcd36dec38da5c981699d4cc6b43724e24a27a
Aia_makhruka <aia.makhruka@gmail.com>
2023-05-22 23:26:47 -0400

    creating and styling the VideoPlayer component

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,11 +1,10 @@
-
-
-function VideoPlayer(){
+import './VideoPlayer.scss'
+function VideoPlayer({PlayingVideo}){
     return(
         <>
         <section className="video-player-container">
-        <video className="video" poster="">
-        </video>
+            <video className="video" controls height="100%"  poster={PlayingVideo.image}>
+            </video>
         </section>
         </>
     )
